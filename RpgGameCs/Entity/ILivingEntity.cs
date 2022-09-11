c0dc45cdc0b76ed3d76c3a82c79647396a5b01fc
BufferOverflow <scharxidev@outlook.de>
2022-09-11 20:43:33 +0200

    added entities

---
+++ RpgGameCs/Entity/ILivingEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace RpgGameCs.Entity;
+
+public interface ILivingEntity : IEntity, IDamageable
+{
+    void kill(IEntity? killer);
+    void heal();
+    void Attack(IEntity target);
+    int GetLastDamage();
+    void SetLastDamage(int amount);
+    Character? GetKiller();
+    bool CanPickupItems(); 
+}
