bcbc2780477dee93cc032ddd9e10b3e105ca254a
BufferOverflow <scharxidev@outlook.de>
2022-09-17 12:24:30 +0200

    added documentation

--- RpgGameCs/Inventory/AbstractInventory.cs
+++ RpgGameCs/Inventory/AbstractInventory.cs
@@ -11,11 +11,22 @@ public abstract class AbstractInventory : IInventory
         _localStorage = new Dictionary<uint, IItem>();
     }
 
+
+    /// <summary>
+    /// Returns the Item found in the slot at the given index
+    /// </summary>
+    /// <param name="slot">The index of the Slot's Item to return</param>
+    /// <returns>The Item in the slot</returns>
     public IItem GetItem(uint slot)
     {
         return _localStorage[slot];
     }
 
+    /// <summary>
+    /// Stores the Item at the given index of the inventory.
+    /// </summary>
+    /// <param name="slot">The index where to put the Item</param>
+    /// <param name="item">The Item to set</param>
     public void SetItem(uint slot, IItem item)
     {
         if (_localStorage.Count == 0)
@@ -26,38 +37,76 @@ public abstract class AbstractInventory : IInventory
         _localStorage[slot] = item;
     }
 
+    /// <summary>
+    /// <para>Removes the given Items from the inventory.</para>
+    /// <para>
+    /// It will try to remove 'as much as possible' from the types and amounts you give as arguments.
+    /// </para>
+    /// <para>
+    /// The returned HashMap contains what it couldn't remove, where the key is the index of the parameter, and the value is the Item at that index of the varargs parameter. If all the given Items are removed, it will return an empty HashMap.
+    /// </para>
+    /// <para>
+    /// It is known that in some implementations this method will also set the inputted argument amount to the number of that item not removed from slots.
+    /// </para>
+    /// </summary>
+    /// <param name="items">The Item to remove</param>
+    /// <returns>A HashMap containing items that couldn't be removed.</returns>
     public Dictionary<uint, IItem> RemoveItem(params IItem[] items)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Clears out a particular slot in the index.
+    /// </summary>
+    /// <param name="index">The index to empty.</param>
     public void Clear(uint index)
     {
         _localStorage.Remove(index);
     }
 
+    /// <summary>
+    /// Clears out the whole Inventory.
+    /// </summary>
     public void Clear()
     {
         _localStorage.Clear();
     }
 
+    /// <summary>
+    /// <para>Removes all stacks in the inventory matching the given stack.</para>
+    /// <para>This will only match a slot if both the type and the amount of the stack match</para>
+    /// </summary>
+    /// <param name="item">The Item to match against</param>
     public void Remove(IItem item)
     {
         _localStorage.ToList().FindAll(pair => pair.Value == item)
             .ForEach(pair => { _localStorage.ToList().Remove(pair); });
     }
 
+    /// <summary>
+    /// Removes all stacks in the inventory matching the given material.
+    /// </summary>
+    /// <param name="material">The material to remove</param>
     public void Remove(Material material)
     {
         _localStorage.ToList().FindAll(pair => pair.Value.MaterialType == material)
             .ForEach(pair => { _localStorage.ToList().Remove(pair); });
     }
 
+    /// <summary>
+    /// Returns all Items from the inventory
+    /// </summary>
+    /// <returns>An Enumerable of Items from the inventory</returns>
     public IEnumerable<IItem> GetContent()
     {
         return _localStorage.Values.ToList();
     }
 
+    /// <summary>
+    /// Completely replaces the inventory's contents. Removes all existing contents and replaces it with the Items given in the array.
+    /// </summary>
+    /// <param name="content">A complete replacement for the contents; the length must be less than or equal to Size.</param>
     public void SetContent(IEnumerable<IItem> content)
     {
         _localStorage.Clear();
@@ -67,6 +116,15 @@ public abstract class AbstractInventory : IInventory
         }
     }
 
+
+    /// <summary>
+    /// <para>Stores the given Items in the inventory. This will try to fill existing stacks and empty slots as well as it can.</para>
+    /// <para>The returned HashMap contains what it couldn't store, where the key is the index of the parameter, and the value is the Item at that index of the varargs parameter. If all items are stored, it will return an empty HashMap.</para>
+    /// <para>If you pass in Items which exceed the maximum stack size for the Material, first they will be added to partial stacks where Material.getMaxStackSize() is not exceeded, up to Material.getMaxStackSize(). When there are no partial stacks left stacks will be split on Inventory.getMaxStackSize() allowing you to exceed the maximum stack size for that material.</para>
+    /// <para>It is known that in some implementations this method will also set the inputted argument amount to the number of that item not placed in slots.</para>
+    /// </summary>
+    /// <param name="items">The Items to add</param>
+    /// <returns>A HashMap containing items that didn't fit.</returns>
     public Dictionary<uint, IItem> AddItem(params IItem[] items)
     {
         throw new NotImplementedException();
--- RpgGameCs/Inventory/ItemContainer.cs
+++ RpgGameCs/Inventory/ItemContainer.cs
@@ -4,17 +4,79 @@ namespace RpgGameCs.Inventory;
 
 public interface IItemContainer
 {
+    /// <summary>
+    /// Returns the Item found in the slot at the given index
+    /// </summary>
+    /// <param name="slot">The index of the Slot's Item to return</param>
+    /// <returns>The Item in the slot</returns>
     IItem GetItem(uint slot);
+
+    /// <summary>
+    /// Stores the Item at the given index of the inventory.
+    /// </summary>
+    /// <param name="slot">The index where to put the Item</param>
+    /// <param name="item">The Item to set</param>
     void SetItem(uint slot, IItem item);
+
+    /// <summary>
+    /// <para>Removes the given Items from the inventory.</para>
+    /// <para>
+    /// It will try to remove 'as much as possible' from the types and amounts you give as arguments.
+    /// </para>
+    /// <para>
+    /// The returned HashMap contains what it couldn't remove, where the key is the index of the parameter, and the value is the Item at that index of the varargs parameter. If all the given Items are removed, it will return an empty HashMap.
+    /// </para>
+    /// <para>
+    /// It is known that in some implementations this method will also set the inputted argument amount to the number of that item not removed from slots.
+    /// </para>
+    /// </summary>
+    /// <param name="items">The Item to remove</param>
+    /// <returns>A HashMap containing items that couldn't be removed.</returns>
     Dictionary<uint, IItem> RemoveItem(params IItem[] items);
 
+    /// <summary>
+    /// Clears out a particular slot in the index.
+    /// </summary>
+    /// <param name="index">The index to empty.</param>
     void Clear(uint index);
-    void Clear(); 
 
+    /// <summary>
+    /// Clears out the whole Inventory.
+    /// </summary>
+    void Clear();
+
+    /// <summary>
+    /// <para>Removes all stacks in the inventory matching the given stack.</para>
+    /// <para>This will only match a slot if both the type and the amount of the stack match</para>
+    /// </summary>
+    /// <param name="item">The Item to match against</param>
     void Remove(IItem item);
+
+    /// <summary>
+    /// Removes all stacks in the inventory matching the given material.
+    /// </summary>
+    /// <param name="material">The material to remove</param>
     void Remove(Material material);
 
+    /// <summary>
+    /// Returns all Items from the inventory
+    /// </summary>
+    /// <returns>An Enumerable of Items from the inventory</returns>
     IEnumerable<IItem> GetContent();
+
+    /// <summary>
+    /// Completely replaces the inventory's contents. Removes all existing contents and replaces it with the Items given in the array.
+    /// </summary>
+    /// <param name="content">A complete replacement for the contents; the length must be less than or equal to Size.</param>
     void SetContent(IEnumerable<IItem> content);
+
+    /// <summary>
+    /// <para>Stores the given Items in the inventory. This will try to fill existing stacks and empty slots as well as it can.</para>
+    /// <para>The returned HashMap contains what it couldn't store, where the key is the index of the parameter, and the value is the Item at that index of the varargs parameter. If all items are stored, it will return an empty HashMap.</para>
+    /// <para>If you pass in Items which exceed the maximum stack size for the Material, first they will be added to partial stacks where Material.getMaxStackSize() is not exceeded, up to Material.getMaxStackSize(). When there are no partial stacks left stacks will be split on Inventory.getMaxStackSize() allowing you to exceed the maximum stack size for that material.</para>
+    /// <para>It is known that in some implementations this method will also set the inputted argument amount to the number of that item not placed in slots.</para>
+    /// </summary>
+    /// <param name="items">The Items to add</param>
+    /// <returns>A HashMap containing items that didn't fit.</returns>
     Dictionary<uint, IItem> AddItem(params IItem[] items);
 }
