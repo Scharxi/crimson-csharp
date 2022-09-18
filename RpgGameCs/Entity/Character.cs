1ed822bf179d9777ecf4f9521193698de3e86bea
BufferOverflow <scharxidev@outlook.de>
2022-09-18 16:55:31 +0200

    improved code base

--- RpgGameCs/Entity/Character.cs
+++ RpgGameCs/Entity/Character.cs
@@ -5,20 +5,20 @@ namespace RpgGameCs.Entity;
 
 public abstract class Character : ILivingEntity, IEquipmentHolder
 {
-    protected Character? Killer;
+    private Character? _killer;
     protected int LastDamage;
     protected uint Health;
-    protected uint AbsorptionAmount;
-    protected uint MaxHealth;
+    private uint _absorptionAmount;
+    private uint _maxHealth;
     protected ILivingEntity? LastDamageDealer;
 
-    private PlayerInventory _inventory;
+    private readonly PlayerInventory _inventory;
 
     protected Character(uint health, uint absorptionAmount, uint maxHealth)
     {
         Health = health;
-        AbsorptionAmount = absorptionAmount;
-        MaxHealth = maxHealth;
+        _absorptionAmount = absorptionAmount;
+        _maxHealth = maxHealth;
         _inventory = new CharacterInventory(this);
         LastDamageDealer = null;
     }
@@ -26,22 +26,22 @@ public abstract class Character : ILivingEntity, IEquipmentHolder
     public abstract void Damage(uint damage);
     public abstract void Damage(uint amount, ILivingEntity source);
 
-    public uint GetAbsorptionAmount() => AbsorptionAmount;
+    public uint GetAbsorptionAmount() => _absorptionAmount;
 
     public uint GetHealth() => Health;
 
-    public uint GetMaxHealth() => MaxHealth;
+    public uint GetMaxHealth() => _maxHealth;
 
     public void SetAbsorptionAmount(uint amount)
     {
-        AbsorptionAmount = amount;
+        _absorptionAmount = amount;
     }
 
     public ILivingEntity? GetLastDamageDealer() => LastDamageDealer;
 
     public void SetMaxHealth(uint maxHealth)
     {
-        MaxHealth = maxHealth;
+        _maxHealth = maxHealth;
     }
 
     public void SetHealth(uint health)
@@ -52,8 +52,8 @@ public abstract class Character : ILivingEntity, IEquipmentHolder
     public void Kill(IEntity? killer)
     {
         if (killer == null)
-            Killer = null;
-        Killer = (Character?)killer!;
+            _killer = null;
+        _killer = (Character?)killer!;
         SetHealth(0);
         SetLastDamage(Math.Abs((int)GetMaxHealth()));
     }
@@ -79,7 +79,7 @@ public abstract class Character : ILivingEntity, IEquipmentHolder
         LastDamage = amount;
     }
 
-    public Character? GetKiller() => Killer;
+    public Character? GetKiller() => _killer;
 
     public bool CanPickupItems() => true;
 
@@ -96,22 +96,6 @@ public abstract class Character : ILivingEntity, IEquipmentHolder
     /// <returns></returns>
     protected uint TakeDamage(uint damage)
     {
-        uint takenDamage;
-        if (AbsorptionAmount != 0)
-        {
-            if ((int)(AbsorptionAmount - damage) < 0)
-            {
-                takenDamage = Health -= damage;
-                return takenDamage;
-            }
-
-            takenDamage = AbsorptionAmount - damage;
-            return takenDamage;
-        }
-        else
-        {
-            takenDamage = Health -= damage;
-            return takenDamage;
-        }
+        throw new NotImplementedException();
     }
 }
